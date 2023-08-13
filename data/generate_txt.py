0c4566fd609de36da286b69c84c65a69b53cce93
Kedreamix <1016617094@qq.com>
2023-08-14 00:03:52 +0800

    生成data.txt文件

--- data/generate_txt.py
+++ data/generate_txt.py
@@ -1,8 +1,11 @@
 import os
+from tqdm import tqdm
 
 # 指定文件夹路径和输出文件名
-folder_path = "/data1/dengkaijun/workdirs/DiffTalk/data/HDTF/images"
-output_file = "/data1/dengkaijun/workdirs/DiffTalk/data/data_train2.txt"
+root = '/data2/dengkaijun/workdirs/DiffTalk/data/HDTF'
+folder_path = os.path.join(root,'images')
+train_output_file = 'data_train.txt'
+test_output_file = 'data_test.txt'
 
 # 获取文件夹中的图片文件列表
 image_files = [file for file in os.listdir(folder_path) if file.lower().endswith(('.png', '.jpg', '.jpeg'))]
@@ -10,16 +13,8 @@ image_files = [file for file in os.listdir(folder_path) if file.lower().endswith
 # 对文件名进行排序
 sorted_image_files = sorted(image_files, key = lambda x: int(x.split('_')[0])*1500 + int(x.split('_')[1][:-4]))
 
-# 将排序后的文件名写入输出文件
-with open(output_file, 'w') as f:
-    for file_name in sorted_image_files:
-        f.write(os.path.basename(file_name).split('.')[0] + '\n')
-
-print("Sorted image names written to", output_file)
-
-
 max_numbers = {}  # 用于存储每个前缀的最大数字
-for file in sorted_image_files:
+for file in tqdm(sorted_image_files):
     file = os.path.basename(file).split('.')[0]
     prefix, number = file.split('_')
     number = int(number)
@@ -34,3 +29,17 @@ with open('data.txt', 'w') as f:
         f.write(f"{prefix} {max_number}\n")
 
 print("Max numbers saved to data.txt")
+
+# DATA_LEN = len(max_numbers)
+# split = 0.5
+# train_len = (DATA_LEN*split)
+
+# 将排序后的文件名写入输出文件
+with open(train_output_file, 'w') as f:
+    with tqdm(total = len(sorted_image_files)) as pbar:
+        for file_name in sorted_image_files:
+            pbar.set_description(os.path.basename(file_name).split('.')[0])
+            f.write(os.path.basename(file_name).split('.')[0] + '\n')
+            pbar.update(1)
+
+print("Sorted image names written to", train_output_file)
