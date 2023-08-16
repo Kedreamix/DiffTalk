c222957254c08780e768a252c5385d266b66e279
Kedreamix <1016617094@qq.com>
2023-08-16 16:34:04 +0800

    Update generate_txt.py

--- data/generate_txt.py
+++ data/generate_txt.py
@@ -3,18 +3,18 @@ from tqdm import tqdm
 
 # 指定文件夹路径和输出文件名
 root = '/data2/dengkaijun/workdirs/DiffTalk/data/HDTF'
-folder_path = os.path.join(root,'images')
+folder_path = os.path.join(root,'audio_smooth')
 train_output_file = 'data_train.txt'
 test_output_file = 'data_test.txt'
 
 # 获取文件夹中的图片文件列表
-image_files = [file for file in os.listdir(folder_path) if file.lower().endswith(('.png', '.jpg', '.jpeg'))]
+files = [file for file in tqdm(os.listdir(folder_path)) if file.lower().endswith(('npy')) and '_' in file]
 
 # 对文件名进行排序
-sorted_image_files = sorted(image_files, key = lambda x: int(x.split('_')[0])*1500 + int(x.split('_')[1][:-4]))
+sorted_files = sorted(files, key = lambda x: int(x.split('_')[0])*1500 + int(x.split('_')[1][:-4]))
 
 max_numbers = {}  # 用于存储每个前缀的最大数字
-for file in tqdm(sorted_image_files):
+for file in tqdm(sorted_files):
     file = os.path.basename(file).split('.')[0]
     prefix, number = file.split('_')
     number = int(number)
@@ -30,16 +30,25 @@ with open('data.txt', 'w') as f:
 
 print("Max numbers saved to data.txt")
 
-# DATA_LEN = len(max_numbers)
-# split = 0.5
-# train_len = (DATA_LEN*split)
-
+train_prefixes = [prefix for prefix, max_number in tqdm(max_numbers.items()) if max_number == 1499]
+test_prefixes = [prefix for prefix, max_number in tqdm(max_numbers.items()) if max_number != 1499]
+# print(train_prefixes)
 # 将排序后的文件名写入输出文件
-with open(train_output_file, 'w') as f:
-    with tqdm(total = len(sorted_image_files)) as pbar:
-        for file_name in sorted_image_files:
-            pbar.set_description(os.path.basename(file_name).split('.')[0])
-            f.write(os.path.basename(file_name).split('.')[0] + '\n')
-            pbar.update(1)
+f_train = open(train_output_file, 'w')
+f_test = open(test_output_file, 'w')
 
-print("Sorted image names written to", train_output_file)
+with tqdm(total = len(sorted_files)) as pbar:
+    for file in sorted_files:
+        pbar.set_description(os.path.basename(file).split('.')[0])
+        pbar.update(1)
+        file_name = os.path.basename(file).split('.')[0]
+        # print(file_name.split('_'))
+        if file_name.split('_')[0] in train_prefixes:
+            f_train.write(file_name + '\n')
+        else:
+            f_test.write(file_name + '\n')
+        
+f_train.close()
+f_test.close()
+
+print("Sorted image names written to", train_output_file, test_output_file)
