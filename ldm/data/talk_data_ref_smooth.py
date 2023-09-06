a50ddfb7c698a3ce2798eeb74ae484e10d7d2752
Kedreamix <1016617094@qq.com>
2023-09-06 17:19:33 +0800

    different mask for train

--- ldm/data/talk_data_ref_smooth.py
+++ ldm/data/talk_data_ref_smooth.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 import random
 import pdb
-
+import cv2
 class TALKBase(Dataset):
     def __init__(self,
                  txt_file,
@@ -31,6 +31,8 @@ class TALKBase(Dataset):
             "landmark_path_": [os.path.join(self.data_root, 'landmarks', l+'.lms') for l in self.image_paths],
             "reference_path": [l.split('_')[0] + '_' + str(random.choice(list(set(range(1, int(self.image_num[int(l.split('_')[0])-1].split()[1])))-set(range(int(l.split('_')[1])-60, int(l.split('_')[1])+60)))))
                                for l in self.image_paths],
+            # "reference_path": [l.split('_')[0] + '_' + str(random.choice(list(set(range(1, 1500))-set(range(int(l.split('_')[1])-60, int(l.split('_')[1])+60)))))
+            #                    for l in self.image_paths],
         }
 
         self.size = size
@@ -67,14 +69,20 @@ class TALKBase(Dataset):
         landmarks_img = np.concatenate((landmarks_img2, landmarks_img))
         scaler = h / self.size
         example["landmarks"] = (landmarks_img / scaler)
-
+        
+        
+        # print(image.shape,"save")
         #mask
-        mask = np.ones((self.size, self.size))
-        mask[(landmarks[30][1] / scaler).astype(int):, :] = 0.
-        mask = mask[..., None]
-        image_mask = (image * mask).astype(np.uint8)
-        example["image_mask"] = (image_mask / 127.5 - 1.0).astype(np.float32)
+        # mask = np.ones((self.size, self.size))
+        # mask[(landmarks[30][1] / scaler).astype(int):, :] = 0.
+        # mask = mask[..., None]
+        # image_mask = (image * mask).astype(np.uint8)
+        mask = face_mask(img.shape, landmarks)
+        image_mask = img*(np.ones_like(mask) - mask) + mask
+        image_mask = np.resize(image_mask,(self.size,self.size,3))
+        # print(image_mask.shape)
 
+        example["image_mask"] = (image_mask / 127.5 - 1.0).astype(np.float32)
         example["audio_smooth"] = np.load(example["audio_smooth_path_"]).astype(np.float32)
 
         #add for reference
@@ -93,8 +101,27 @@ class TALKBase(Dataset):
 
 class TalkTrain(TALKBase):
     def __init__(self, **kwargs):
-        super().__init__(txt_file="./data/my_data_train.txt", data_root="./data/HDTF", **kwargs)
+        super().__init__(txt_file="./data/data_train.txt", data_root="./data/HDTF", **kwargs)
 
 class TalkValidation(TALKBase):
     def __init__(self, flip_p=0., **kwargs):
-        super().__init__(txt_file="./data/my_data_test.txt", data_root="./data/HDTF", flip_p=flip_p, **kwargs)
+        super().__init__(txt_file="./data/data_test.txt", data_root="./data/HDTF", flip_p=flip_p, **kwargs)
+
+def show_image(img, output_path, title=""):
+    import matplotlib.pyplot as plt
+    # Create a figure and axis
+    fig, ax = plt.subplots(1, figsize=(8, 8))
+    ax.imshow(img, cmap = 'gray')
+    ax.set_title(title)
+    plt.axis('off')
+    plt.savefig(output_path, bbox_inches='tight', pad_inches=0)
+
+def face_mask(img_shape, landmark_list, dtype='uint8'):
+    height, width = img_shape[:2]
+    mask = np.zeros((height, width, 1), dtype=dtype)
+    cv2.drawContours(mask, np.int32([landmark_list[2:15]]), -1, color=(1), thickness=cv2.FILLED)
+    # cv2.polylines(mask, np.int32([landmark_list[48:60]]), isClosed=False, color=(0), thickness=1)  # 'Outer Mouth'
+    # cv2.polylines(mask, np.int32([landmark_list[60:68]]), isClosed=False, color=(0), thickness=1)  # 'Inner Mouth'
+    # cv2.line(mask, np.int32(landmark_list[48]), np.int32(landmark_list[59]), color=(0), thickness=1)
+    # cv2.line(mask, np.int32(landmark_list[60]), np.int32(landmark_list[67]), color=(0), thickness=1)
+    return mask
